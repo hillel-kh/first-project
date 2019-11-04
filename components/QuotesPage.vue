aeb76172e6558514d500bdc4335ea70b85e749da
Alex Litvinov <litvinov1618@gmail.com>
2019-11-04 22:38:32 +0200

    added quote app

---
+++ components/QuotesPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <ProjectLayout title="Quotes" bgcolor="#FCF6B1" color="#2D1E2F">
+        <Quotes></Quotes>
+    </ProjectLayout>
+</template>
+
+<script>
+import Quotes from './quotes/Quotes'
+import ProjectLayout from './ProjectLayout.vue'
+
+export default {
+    components: {
+        Quotes,
+        ProjectLayout
+    }     
+}
+</script>
