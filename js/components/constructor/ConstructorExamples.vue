21d9dcbe8936f22153e33ef6d63facbd319c0f5d
Viktor Ovcharenko <vikt.ovcharenko@gmail.com>
2019-11-08 00:30:01 +0200

    add preview mocup

--- js/components/constructor/ConstructorExamples.vue
+++ js/components/constructor/ConstructorExamples.vue
@@ -3,7 +3,11 @@
 		<h2>ConstructorExamples</h2>
 		<ul v-if="templates.length"
 		    class="templates-list">
-			<li v-for="template in templates"></li>
+			<li v-for="template in templates">
+				<div class="example">
+					<a href="">preview</a>
+				</div>
+			</li>
 		</ul>
 		<div v-else>
 			<p>Be the first, create a template!</p>
@@ -14,7 +18,7 @@
 <script>
 	export default {
 		data: () => ({
-			templates: [1]
+			templates: [1,2,3]
 		}),
 
 		methods: {
@@ -30,6 +34,12 @@
 
 	.templates-list {
 		@extend %listreset;
+
+		.example {
+			margin: 10px;
+			padding: 10px;
+			border: 1px solid #000;
+		}
 	}
 
 </style>
--- js/components/constructor/ConstructorPreview.vue
+++ js/components/constructor/ConstructorPreview.vue
@@ -1,5 +1,14 @@
 <template>
 	<div>
-		<h2>ConstructorPreview</h2>
+		<header>
+			<h2>ConstructorPreview</h2>
+			<nav>
+				<button>Home page</button>
+				<button>Edit</button>
+			</nav>
+		</header>
+		<main>
+			some view
+		</main>
 	</div>
 </template>
