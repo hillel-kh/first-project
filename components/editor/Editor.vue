dc068993c40c9dca4592ebae6069f3c99f6a2370
Alex Litvinov <litvinov1618@gmail.com>
2019-11-10 14:56:29 +0200

    deleted tabs

--- components/editor/Editor.vue
+++ components/editor/Editor.vue
@@ -1,18 +1,17 @@
 <template>
-    <CenteredLayout>
-        <div class="wrapper">
-            <h2 class="title">Текстовый редактор с автосохранением</h2>
-            <div class="buttons">
-              <button>TABLE</button>
-              <button><b>BOLD</b></button>
-              <button ><i>ITALICS</i></button>
-              <button>IMAGE</button>
-            </div>
-            <div class="editor" contenteditable="true"></div>
-        </div>
-    </CenteredLayout>
+  <CenteredLayout>
+    <div class="wrapper">
+      <h2 class="title">Текстовый редактор с автосохранением</h2>
+      <div class="buttons">
+        <button>TABLE</button>
+        <button><b>BOLD</b></button>
+        <button ><i>ITALICS</i></button>
+        <button>IMAGE</button>
+      </div>
+      <div class="editor" contenteditable="true"></div>
+    </div>
+  </CenteredLayout>
 </template>
-
 
 <style scoped>
 body{ 
@@ -26,34 +25,31 @@ body{
   margin-bottom: 20px;
 }
 .editor{  
-    padding: 1em;
-    background: #E6E6E6;
-    border-radius: 3px;
-    height: 50%; 
-    text-align: center;
-    margin: 10;
-    font-family: Verdana, Arial, sans-serif;
-    font-size: 24px;
-    border: 1px solid;
+  padding: 1em;
+  background: #E6E6E6;
+  border-radius: 3px;
+  height: 50%; 
+  text-align: center;
+  margin: 10;
+  font-family: Verdana, Arial, sans-serif;
+  font-size: 24px;
+  border: 1px solid;
 }
 .title {
-    color:navy;
-    text-align: center;
-    font-size: 35px;
-    margin-top: 5px;
+  color:navy;
+  text-align: center;
+  font-size: 35px;
+  margin-top: 5px;
 }
-
 button{
-    /* height:80px;
-    width:150px; */
-    padding: 15px 10px;
+  /* height:80px;
+  width:150px; */
+  padding: 15px 10px;
 }
-
 .headline{
   font-size: 2em;
   text-align: center;
 }
-
 .wrapper {
   width: 100%;
   background: #FFF;
@@ -61,13 +57,8 @@ button{
   box-shadow: 0 2px 5px rgba(0,0,0,0.3);
   border-radius: 3px;
 }
-
-
-
 button:hover, button:focus {
   cursor: pointer;
   outline: none;
 }
-
-
 </style>
