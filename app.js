5e50d567e21a472ec2e34361bc1ebefa05a220b8
K0TLIAR0VA <1914820@gmail.com>
2019-11-12 12:46:10 +0200

    fixed according to remark

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import App from './components/Application.vue';
-import './styles/main.css';
 import './sass/reset.sass';
+import './styles/main.css';
+
 
 const app = new Vue(App);
 window.app = app.$mount('#app');
